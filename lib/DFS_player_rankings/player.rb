920ede22010841fd433f3ac18cf179b44f092efb
ChristopherJamesN <nady.christopher@gmail.com>
2017-09-19 15:19:03 -0500

    Updated player and scraper classes

--- lib/DFS_player_rankings/player.rb
+++ lib/DFS_player_rankings/player.rb
@@ -1,2 +1,37 @@
 class DFSPlayerRankings::Player
+
+
+  attr_accessor :name, :rank, :totalpoints, :url
+
+  @@all = []
+
+  def self.new_from_index_page(r)
+    self.new(
+      r.css("td.player a").text,
+      "https://rotogrinders.com/rankings#{r.css("td.player a").attribute("href").text}",
+      r.css("td.rank").text,
+      r.css("td.points").text
+      )
+  end
+
+  def initialize(name=nil, url=nil, rank=nil, totalpoints=nil)
+    @name = name
+    @url = url
+    @rank = rank
+    @totalpoints = totalpoints
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.find(id)
+    self.all[id-1]
+  end
+
+  def doc
+    @doc ||= Nokogiri::HTML(open(self.url))
+  end
+
 end
--- lib/DFS_player_rankings/scraper.rb
+++ lib/DFS_player_rankings/scraper.rb
@@ -1,2 +1,17 @@
 class DFSPlayerRankings::Scraper
+
+  def get_page
+    Nokogiri::HTML(open("https://rotogrinders.com/rankings"))
+  end
+
+  def scrape_players_index
+    self.get_page.css("table.tbl rankings grinders")
+  end
+
+  def make_players
+    scrape_players_index.each do |r|
+      DFSPlayerRankings::Player.new_from_index_page(r)
+    end
+  end
+
 end
