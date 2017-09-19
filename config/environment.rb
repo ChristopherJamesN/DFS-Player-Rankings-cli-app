ce784968559caf527e82f680ff8b3dd2802a1121
ChristopherJamesN <nady.christopher@gmail.com>
2017-09-19 14:13:51 -0500

    Added config folder and envionment.rb file

---
+++ config/environment.rb
@@ -0,0 +1,7 @@
+require 'pry'
+require 'nokogiri'
+require 'open-uri'
+
+require_relative '../lib/DFS_player_rankings/scraper'
+require_relative '../lib/DFS_player_rankings/player'
+require_relative '../lib/DFS_player_rankings/cli'
