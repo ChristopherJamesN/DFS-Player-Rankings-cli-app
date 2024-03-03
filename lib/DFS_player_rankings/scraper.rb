90dbef31ec8982f9f8722ecd3f542ec55e9a411d
ChristopherJamesN <nady.christopher@gmail.com>
2024-03-03 17:30:22 -0600

    Update selector paths for `scraper.rb`

--- lib/DFS_player_rankings/scraper.rb
+++ lib/DFS_player_rankings/scraper.rb
@@ -1,19 +1,17 @@
 class DFSPlayerRankings::Scraper
-
   def get_page
-    Nokogiri::HTML(URI.open("https://rotogrinders.com/rankings"))
+    Nokogiri::HTML(URI.open('https://rotogrinders.com/rankings'))
   end
 
   def scrape_players_index
-    self.get_page.css("tbody tr")
+    get_page.css('tbody tr')
   end
 
   def make_players
     scrape_players_index.each do |r|
-      DFSPlayerRankings::Player.new(r.css("td.player a").text,
-      r.css("td.rank").text.strip,
-      r.css("td.points").text.strip)
+      DFSPlayerRankings::Player.new(r.css('body > section > div.container.ranking > div:nth-child(3) > table > tbody > tr:nth-child(1) > td.ranking-table-avatar.left > a').text,
+                                    r.css('body > section > div.container.ranking > div:nth-child(3) > table > tbody > tr:nth-child(1) > td.gold.min.center.bold').text.strip,
+                                    r.css('body > section > div.container.ranking > div:nth-child(3) > table > tbody > tr:nth-child(1) > td:nth-child(5)').text.strip)
     end
   end
-
 end
